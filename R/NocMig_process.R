0d7ff07bd901420b19d33a45350e9c884b74046a
Meinolf Ottensmann <mottensmann@users.noreply.github.com>
2023-10-02 17:41:52 +0200

    small fixes

--- R/NocMig_process.R
+++ R/NocMig_process.R
@@ -1,37 +1,42 @@
 #' Process NocMig session for downstream analysis
 #'
-#' @param path path to NocMig folders
-#' @param folder folder to process
+#' @param parent.folder path
+#' @param child.folder optional. defaults to \code{NULL}
 #' @param rename logical. defaults to \code{TRUE}
+#' @param segment_length optional numeric value. If specified, function \code{\link{split_wave}} is used to to segmenti long audio files in shorter segments. defaults to \code{NULL}
 #' @inheritParams rename_recording
 #' @inheritParams NocMig_meta
 #' @export
 #'
 NocMig_process <- function(
-    path = "E:/NocMig", folder = NULL,
+    parent.folder = "E:/NocMig", child.folder = "",
     format = c("wav", "mp3"),
     rename = TRUE,
     ctime = c("first", "each"),
     lat = 52.032090,
-    lon = 8.516775) {
+    lon = 8.516775,
+    segment_length = NULL) {
+
+  format <- match.arg(format)
+  ctime <- match.arg(ctime)
 
   ## 01: check path is valid
   ## ---------------------------------------------------------------------------
-  if (!dir.exists(file.path(path, folder))) {
-    cat(file.path(path, folder), "not found!\n")
+  if (!dir.exists(file.path(parent.folder, child.folder))) {
+    cat(file.path(parent.folder, child.folder), "not found!\n")
   }
 
   ## 02: Rename Recording (not touching files with proper name!)
   ## ---------------------------------------------------------------------------
   if (isTRUE(rename)) {
-    cat("Rename files in", file.path(path, folder), "\n")
+    cat("Rename files in", file.path(parent.folder, child.folder), "\n")
     output <- rename_recording(
-      path = file.path(path, folder),
+      path = file.path(parent.folder, child.folder),
       ctime = ctime,
       format = format)
     output.time <- RecreateDateTime(output$new.name)
   } else {
-    output <- list.files(path = file.path(path, folder),
+    output <- list.files(path = file.path(parent.folder, child.folder),
                          pattern = paste0(".", format),
                          full.names = F,
                          ignore.case = T)
@@ -40,9 +45,22 @@ NocMig_process <- function(
 
   ## 03: Obtain NocMig header
   ## ---------------------------------------------------------------------------
-  cat("Write NocMig head to file", file.path(path, folder, "NocMig_meta.txt"), "\n")
-  sink(file.path(path, folder, "NocMig_meta.txt"))
+  cat("Write NocMig head to file", file.path(parent.folder, child.folder, "NocMig_meta.txt"), "\n")
+  sink(file.path(parent.folder, child.folder, "NocMig_meta.txt"))
   NocMig_meta(lat = lat, lon = lon, date = lubridate::as_date(min(output.time)))
   sink()
+
+  if (!is.null(segment_length) & methods::is(segment_length, "numeric")) {
+    if (format == "mp3") {
+      stop("Segementing audio files required wave files - mp3 is not supported")
+    } else {
+      audio.files <- list.files(file.path(parent.folder, child.folder),
+                                pattern = format, ignore.case = TRUE)
+      lapply(audio.files, split_wave,
+             path = file.path(parent.folder, child.folder),
+             segment = segment_length, discard_input = T)
+    }
+
+  }
 }
 
