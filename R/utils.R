11ca9b3a711ca983c53ec51420383cc5065ef110
Meinolf Ottensmann <mottensmann@users.noreply.github.com>
2026-06-11 15:45:35 +0200

    adding NocMigR2::read.audacity to replace seewave::read_audacity

--- R/utils.R
+++ R/utils.R
@@ -795,3 +795,26 @@ reformat_xlsx <- function(path) {
   # Save workbook
   openxlsx::saveWorkbook(wb, file.path(path, "BirdNET.xlsx"), overwrite = TRUE)
 }
+
+#' Replacement for \code{\link[seewave]{read.audacity}} to avoid problems with certain labels containing apostrophes etc.
+#'
+#' @description
+#' Read content of a text file and export in format matching \code{\link[seewave]{read.audacity}}
+#'
+#'
+#' @param file A .txt file
+#' @param delim delim
+#' @param col_names col_names
+#' @importFrom readr read_delim
+#' @export
+#'
+read.audacity <- function(file, delim = '\t', col_names = c("start", "end", "label", "score")) {
+  df <- readr::read_delim(file = file,
+                          delim = delim,
+                          col_names = col_names,
+                          show_col_types = FALSE,
+                          progress = FALSE)
+
+  df <- rbind(data.frame(file = file, df))
+  return(df)
+}
